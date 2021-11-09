08a339c3b0abfbd2ba1d33948391836353f44278
MoathAlkawaja <93875570+MoathAlkawaja@users.noreply.github.com>
2021-11-09 18:42:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+// Q1] Predict the value of the following expressions then try them out in the console
+
+9 + 1 * 5; // 14
+9 % 2; // 1
+(9 / 3) * (10 - 7); // 9 
+21 / 3 + 3 * 9 * 9 + 5; // 815
+36 / 9 + 48 - 10 / 2; // 47
+console.log(9+1*5);
+console.log(9 % 2);
+console.log((9 / 3) * (10 - 7));
+console.log(21 / 3 + 3 * 9 * 9 + 5);
+console.log(36 / 9 + 48 - 10 / 2);
+
+//Q2] Figure out the syntax errors in the following variable declarations, and fix them.
+
+const name = 'John'; // using (@) in declaration.
+const'age'=10; // using single cotation
+const const = "Constant"; // const is reserved word
+const variable_1 = "value"; // using space 
+
+//Q3] Define the following variables.
+let color="white";
+let positiveNumber = 24;
+let PhoneNumber = 0787582875;
+
+// Q4]Before trying the following expressions try to predict the outcome.
+10 + "10"; // 1010
+"10" + "10"; // 1010
+10 + 10 + "5"; // 205
+"Hello" + 1; //Hello1
+"10" + 5 + 5; // 1010
+"20" - 10; // 10
+"9" - "5"; // 4
+"19" - "13" + 17; // 23
+5 + 6 + "4" + 9 - 4 - 2; // 1143
